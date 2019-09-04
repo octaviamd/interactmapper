bb47172f022edbadfd1c81bc35e214261b60fbf0
octaviamd <octavia-maria.dancu@mail.mcgill.ca>
2019-09-04 00:10:59 -0400

    adding the imports to the description and editing the :: package info in interact quant

--- R/interact_quant.R
+++ R/interact_quant.R
@@ -23,7 +23,7 @@ interact_quant=function(count_data, quant_info_name, quant_info, dim_red_meth, y
     dim_data<-pca_maker(count_data)
   }
   dim_data<-color_me_grad(dim_data, quant_info, your_palette)
-  p1<-plot_ly(dim_data, x = dim_data[,1], y = dim_data[,2], type = 'scatter', mode = 'markers', marker=list(color= ~colours),
+  p1<-plotly::plot_ly(dim_data, x = dim_data[,1], y = dim_data[,2], type = 'scatter', mode = 'markers', marker=list(color= ~colours),
               hoverinfo = 'text', text = ~paste('Sample Name: ', dim_data$sample.labels, '<br>', quant_info_name, ':', quant_info))
   
   return(p1)
