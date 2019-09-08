ce282b3cf3976f072e67d76cc4c6d6b4fdc3de81
octaviamd <octavia-maria.dancu@mail.mcgill.ca>
2019-09-08 16:10:21 -0400

    ok, working a lot better, but not displaying any title for the secondary row when secdary info name isn't provided. Trying to set up a default name in that case

--- R/interact_multi.R
+++ R/interact_multi.R
@@ -39,7 +39,12 @@ interact_multi=function(count_data, main_info_data, sec_info_data, dim_red_meth,
     }
   }
   if(missing(sec_info_name)==TRUE){
+    if(is.vector(sec_info_data)==TRUE){
+      sec_info_name<-"Secondary info:"
+    }
+    else{
     sec_info_name<-colnames(sec_info_data)
+    }
   }
   a<-c()
   print(str(sec_info_data))
