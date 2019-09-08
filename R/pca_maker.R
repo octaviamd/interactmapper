33d9775436b188560645fbfc7bd56dd2e384924d
octaviamd <octavia-maria.dancu@mail.mcgill.ca>
2019-09-08 19:38:02 -0400

    more tinkering with the help pages of color me grad and pca_maker

--- R/pca_maker.R
+++ R/pca_maker.R
@@ -1,12 +1,12 @@
 #' @title pca_maker
 #' @description Returns a dataframe with the two feature columns resulting from the PCA (principal components analysis) dimension reduction and a sample names column
 #' @param count_data  A dataframe with the input data
-#' @return a dataframe with the two feature columns resulting from the PCA dimension reduction and a sample names column
+#' @return a dataframe with the two feature columns resulting from the PCA dimension reduction and a sample names column (where sample names are obtained from the rownames of count_data)
 #' @details 
 #' @examples 
 #'  p<-pca_maker(iris[,1:4])
 #' @seealso 
-#'  \code{\link[prcomp]{stats}}
+#'  \code{\link[prcomp]{prcomp}}
 #' @rdname pca_maker
 #' @export 
 pca_maker=function(count_data){
