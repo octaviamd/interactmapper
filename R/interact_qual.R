89e98ded9c3457002710c0bea39a059a6e65f798
octaviamd <octavia-maria.dancu@mail.mcgill.ca>
2019-09-09 20:49:34 -0400

    edited the help page for multi, tried to improve info on hover for interact qual

--- R/interact_qual.R
+++ R/interact_qual.R
@@ -19,7 +19,7 @@ interact_qual=function(count_data, qual_data, dim_red_meth=c("UMAP", "PCA")){
   if(toupper(dim_red_meth)=="PCA"){
     dim_data<-pca_maker(count_data)
   }
-  sample_ggplot <- ggplot2::ggplot(dim_data, ggplot2::aes(x=dim_data[,1], y=dim_data[,2], colour=qual_data)) + ggplot2::geom_point()
-  umap_plot<-plotly::ggplotly(sample_ggplot)
+  sample_ggplot <- ggplot2::ggplot(dim_data, ggplot2::aes(x=dim_data[,1], y=dim_data[,2], colour=qual_data, sample.name=dim_data$sample.labels)) + ggplot2::geom_point()
+  umap_plot<-plotly::ggplotly(sample_ggplot, tooltip = c("sample.name", "colour"))
   return(umap_plot)
 }
